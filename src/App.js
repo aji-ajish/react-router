e83f5adddcd4da5a721600fa8bc8063e87cbe8a6
Ajish s <ajish.r@infospica.com>
2023-05-16 17:53:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './Header'
+import Nav from './Nav'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Missing from './Missing'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost/>
+      <PostPage/>
+      <About/>
+      <Missing/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
